7d5befc9096c182b413eff5baa59b02ce382177b
Ismaxis <Ismaxis@github.com>
2023-08-23 21:07:02 +0300

    * Server setup moved from Phase handler

--- src/main/discord/phases/PresidentPickingPhaseHandler.java
+++ src/main/discord/phases/PresidentPickingPhaseHandler.java
@@ -1,37 +1,16 @@
 package discord.phases;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import discord.DiscordIODevice;
 import discord.entities.DiscordMember;
 import discord.entities.DiscordTeamProperty;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.concrete.Category;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import discord.util.ServerSetupUtil;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu.Builder;
-import net.dv8tion.jda.api.requests.RestAction;
-import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
-import net.dv8tion.jda.api.requests.restaction.ChannelAction;
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
-import net.dv8tion.jda.api.requests.restaction.RoleAction;
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
-import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import social_logic.Session;
-import social_logic.entities.IMember;
 import social_logic.entities.TeamBuilder;
 import social_logic.phases.handlers_interfaces.IPresidentPickingPhaseEventHandler;
 import social_logic.phases.logic.PresidentPickingPhaseLogic;
-import util.Constants;
 
 public class PresidentPickingPhaseHandler extends ADiscordPhaseEventHandler
         implements IPresidentPickingPhaseEventHandler {
@@ -48,96 +27,10 @@ public class PresidentPickingPhaseHandler extends ADiscordPhaseEventHandler
             properties.add(null);
         }
 
-        createChannelsAndRoles().complete();
-        sendPolls().complete();
+        ServerSetupUtil util = new ServerSetupUtil(getJDA(), builders, properties);
+        util.createChannelsAndRoles(session.getIODevice().getGuildId()).complete();
+        util.sendPolls().complete();
         scheduleEnd();
-    }
-
-    private static final String pickPresident = "votePresident";
-
-    private RestAction<?> sendPolls() {
-        List<MessageCreateAction> actions = new ArrayList<>(phaseLogic.getTeamCount());
-        for (int i = 0; i < phaseLogic.getTeamCount(); i++) {
-            Set<IMember> members = phaseLogic.getTeamBuilder(i).getMembers();
-            Builder menuBuilder = StringSelectMenu.create(pickPresident + i); // id of interaction
-            for (IMember m : members) {
-                User user = getJDA().getUserById(m.getID());
-                menuBuilder.addOption(user.getName(), user.getId());
-            }
-
-            MessageCreateData message = new MessageCreateBuilder()
-                    .setContent("Pick your president!")
-                    .addActionRow(menuBuilder.build())
-                    // TODO: remove debug button
-                    .addActionRow(Button.of(ButtonStyle.PRIMARY, "proceedVotes" + i, "[DEBUG] Proceed votes"))
-                    .build();
-
-            MessageCreateAction sendMessage = getJDA().getVoiceChannelById(properties.get(i).voiceChatID())
-                    .sendMessage(message);
-            actions.add(sendMessage);
-        }
-        return RestAction.allOf(actions);
-    }
-
-    private RestAction<?> createChannelsAndRoles() {
-        Guild guild = getJDA().getGuildById(session.getIODevice().getGuildId());
-        Category category = guild.createCategory(Constants.bundle.getString("game_name")).complete();
-        List<Role> roles = createRoles(guild).complete();
-
-        List<RestAction<?>> actions = new ArrayList<>(phaseLogic.getTeamCount());
-
-        for (int i = 0; i < phaseLogic.getTeamCount(); i++) {
-            Role role = roles.get(i);
-            final int index = i;
-            final long roleId = role.getIdLong();
-            TeamBuilder builder = phaseLogic.getTeamBuilder(i);
-            RestAction<List<Void>> addMembersToRole = addMembersToRole(role, builder.getMembers());
-            RestAction<VoiceChannel> createVC = createVoiceTeamChannel(category, role,
-                    builder.getDescription().getName())
-                            .onSuccess(vc -> properties.set(index, new DiscordTeamProperty(vc.getIdLong(), roleId)));
-            actions.add(addMembersToRole);
-            actions.add(createVC);
-        }
-
-        return RestAction.allOf(actions);
-    }
-
-    private RestAction<List<Role>> createRoles(Guild guild) {
-        List<RoleAction> actions = new ArrayList<>(phaseLogic.getTeamCount());
-        for (int i = 0; i < phaseLogic.getTeamCount(); i++) {
-            TeamBuilder builder = phaseLogic.getTeamBuilder(i);
-            RoleAction action = guild.createRole()
-                    .setName(builder.getDescription().getName())
-                    .setColor(builder.getDescription().getColor())
-                    .setMentionable(true);
-
-            actions.add(action);
-        }
-        return RestAction.allOf(actions);
-    }
-
-    private RestAction<List<Void>> addMembersToRole(Role role, Set<? extends IMember> members) {
-        List<AuditableRestAction<Void>> actions = new ArrayList<>(members.size());
-        for (IMember m : members) {
-            AuditableRestAction<Void> action = role.getGuild()
-                    .addRoleToMember(getJDA().getUserById(m.getID()), role)
-                    .reason("Added role by World Domination Game");
-            actions.add(action);
-        }
-        return RestAction.allOf(actions);
-    }
-
-    private ChannelAction<VoiceChannel> createVoiceTeamChannel(Category category, Role role, String name) {
-        Guild guild = category.getGuild();
-        return category.createVoiceChannel(name)
-                .addRolePermissionOverride(
-                        guild.getPublicRole().getIdLong(),
-                        0,
-                        Permission.VOICE_CONNECT.getRawValue())
-                .addRolePermissionOverride(
-                        role.getIdLong(),
-                        Permission.VOICE_CONNECT.getRawValue(),
-                        0);
     }
 
     @Override
---
+++ src/main/discord/util/ServerSetupUtil.java
@@ -0,0 +1,128 @@
+package discord.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import discord.entities.DiscordTeamProperty;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu.Builder;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
+import net.dv8tion.jda.api.requests.restaction.ChannelAction;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+import net.dv8tion.jda.api.requests.restaction.RoleAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import social_logic.entities.IMember;
+import social_logic.entities.TeamBuilder;
+import util.Constants;
+
+public class ServerSetupUtil {
+    private final JDA jda;
+    private final List<TeamBuilder> teamBuilders;
+    private final ArrayList<DiscordTeamProperty> properties;
+
+    private static final String pickPresident = "votePresident";
+
+    public ServerSetupUtil(JDA jda, List<TeamBuilder> teamBuilders, ArrayList<DiscordTeamProperty> properties) {
+        this.jda = jda;
+        this.teamBuilders = teamBuilders;
+        this.properties = properties;
+    }
+
+    public RestAction<?> sendPolls() {
+        List<MessageCreateAction> actions = new ArrayList<>(teamBuilders.size());
+        for (int i = 0; i < teamBuilders.size(); i++) {
+            Set<IMember> members = teamBuilders.get(i).getMembers();
+            Builder menuBuilder = StringSelectMenu.create(pickPresident + i); // id of interaction
+            for (IMember m : members) {
+                User user = jda.getUserById(m.getID());
+                menuBuilder.addOption(user.getName(), user.getId());
+            }
+
+            MessageCreateData message = new MessageCreateBuilder()
+                    .setContent("Pick your president!")
+                    .addActionRow(menuBuilder.build())
+                    // TODO: remove debug button
+                    .addActionRow(Button.of(ButtonStyle.PRIMARY, "proceedVotes" + i, "[DEBUG] Proceed votes"))
+                    .build();
+
+            MessageCreateAction sendMessage = jda.getVoiceChannelById(properties.get(i).voiceChatID())
+                    .sendMessage(message);
+            actions.add(sendMessage);
+        }
+        return RestAction.allOf(actions);
+    }
+
+    public RestAction<?> createChannelsAndRoles(long guildId) {
+        Guild guild = jda.getGuildById(guildId);
+        Category category = guild.createCategory(Constants.bundle.getString("game_name")).complete();
+        List<Role> roles = createRoles(guild).complete();
+
+        List<RestAction<?>> actions = new ArrayList<>(teamBuilders.size());
+
+        for (int i = 0; i < teamBuilders.size(); i++) {
+            Role role = roles.get(i);
+            final int index = i;
+            final long roleId = role.getIdLong();
+            TeamBuilder builder = teamBuilders.get(i);
+            RestAction<List<Void>> addMembersToRole = addMembersToRole(role, builder.getMembers());
+            RestAction<VoiceChannel> createVC = createVoiceTeamChannel(category, role,
+                    builder.getDescription().getName())
+                            .onSuccess(vc -> properties.set(index, new DiscordTeamProperty(vc.getIdLong(), roleId)));
+            actions.add(addMembersToRole);
+            actions.add(createVC);
+        }
+
+        return RestAction.allOf(actions);
+    }
+
+    public RestAction<List<Role>> createRoles(Guild guild) {
+        List<RoleAction> actions = new ArrayList<>(teamBuilders.size());
+        for (int i = 0; i < teamBuilders.size(); i++) {
+            TeamBuilder builder = teamBuilders.get(i);
+            RoleAction action = guild.createRole()
+                    .setName(builder.getDescription().getName())
+                    .setColor(builder.getDescription().getColor())
+                    .setMentionable(true);
+
+            actions.add(action);
+        }
+        return RestAction.allOf(actions);
+    }
+
+    public RestAction<List<Void>> addMembersToRole(Role role, Set<? extends IMember> members) {
+        List<AuditableRestAction<Void>> actions = new ArrayList<>(members.size());
+        for (IMember m : members) {
+            AuditableRestAction<Void> action = role.getGuild()
+                    .addRoleToMember(jda.getUserById(m.getID()), role)
+                    .reason("Added role by World Domination Game");
+            actions.add(action);
+        }
+        return RestAction.allOf(actions);
+    }
+
+    public ChannelAction<VoiceChannel> createVoiceTeamChannel(Category category, Role role, String name) {
+        Guild guild = category.getGuild();
+        return category.createVoiceChannel(name)
+                .addRolePermissionOverride(
+                        guild.getPublicRole().getIdLong(),
+                        0,
+                        Permission.VOICE_CONNECT.getRawValue())
+                .addRolePermissionOverride(
+                        role.getIdLong(),
+                        Permission.VOICE_CONNECT.getRawValue(),
+                        0);
+    }
+
+}
